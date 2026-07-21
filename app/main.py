ec03c35600349c76187dd3ede3c3009c401c9d64
Rakshit <rakshit@nvassociates.co>
2026-07-21 11:42:20 +0530

    Origin added

--- app/main.py
+++ app/main.py
@@ -14,6 +14,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://localhost:5173",
+        "https://graphics-ui-frontend.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
