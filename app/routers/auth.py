7828c1caf39bc0a7201de67a811042487221db92
Rakshit <rakshit@nvassociates.co>
2026-07-23 11:49:44 +0530

    Secured

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -23,7 +23,7 @@ def _set_refresh_cookie(response: Response, refresh_token: str):
         key=REFRESH_COOKIE_NAME,
         value=refresh_token,
         httponly=True,
-        secure=False,      # set True once you're on HTTPS in production
+        secure=True,      # set True once you're on HTTPS in production
         samesite="lax",
         max_age=REFRESH_COOKIE_MAX_AGE,
         path="/",
